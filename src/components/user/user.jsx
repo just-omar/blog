d7f133e781c8e9aa12de7dd950ab754188502b98
omar-home-dekstop <medzhidov.omar11@gmail.com>
2024-03-21 21:23:20 +0300

    broken image

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,6 +1,8 @@
 import { format } from 'date-fns';
 import PropTypes from 'prop-types';
 
+import defaultAvatar from '../../assets/avatarDefault.svg';
+
 import styles from './user.module.scss';
 
 function User({ username, image, createDate }) {
@@ -11,13 +13,22 @@ function User({ username, image, createDate }) {
         <span className={styles.user__username}>{username}</span>
         {createDate ? <span className={styles.user__date}>{format(date, 'MMMM d, yyyy')}</span> : null}
       </div>
-      <img className={styles.user__img} alt="User Avatar" src={image} />
+      <img
+        className={styles.user__img}
+        alt="User Avatar"
+        src={image}
+        onError={(evt) => {
+          evt.target.src = defaultAvatar;
+        }}
+      />
     </div>
   );
 }
 
 User.defaultProps = {
   image: 'https://static.productionready.io/images/smiley-cyrus.jpg',
+  //  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmBPheiaW5dpcrADIIQvQ-NXS2t_0hz5muHpQvWpXDjw&s', -  valid
+  // https://asdasdasdasda.com/ - invalid
   createDate: null,
 };
 
@@ -26,5 +37,4 @@ User.propTypes = {
   image: PropTypes.string,
   createDate: PropTypes.string,
 };
-
 export default User;
