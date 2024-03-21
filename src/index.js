728a7e80cbaaca7c56d022260a552d1505c88af0
omar-home-dekstop <medzhidov.omar11@gmail.com>
2024-03-21 17:36:36 +0300

    markdown and direct links

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import { store } from './redux/store.js';
 import App from './components/app/app.jsx';
@@ -9,8 +10,8 @@ import App from './components/app/app.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>
 );
